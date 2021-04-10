d0e5513580fe98ed43856eb51c18070aadbb2267
VaradMash <VaradMash@users.noreply.github.com>
2021-04-10 13:02:46 +0530

    .gitignore file updated. node_modules folder excluded.

---
+++ app.js
@@ -0,0 +1,31 @@
+//Define database end point.
+const db = firebase.firestore();
+//Reset database settings.
+db.settings({ timestampsInSnaphsots: true });
+//Get elements from html document.
+const form = document.getElementById('new_blog_form');
+const submit_button = document.getElementById('submit_button');
+//Define month array
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+//define date object
+var date = new Date();
+
+submit_button.onclick = function (event)
+{
+    //Prevent Default refresh routine.
+    event.preventDefault();
+    //Generate current date.
+    const d = date.getDate().toString() + " " + months[date.getMonth()] + ", " +  date.getFullYear().toString();
+    //Post data to database.
+    db.collection('blogs').add({
+      title: form.blog_title.value,
+      post_date:d,
+      tags: form.blog_tags.value,
+      author: form.blog_author.value,
+      category: form.blog_category.value,
+      summary: form.blog_summary.value,
+      content: form.blog_content.value
+    })
+    alert("Blog Posted Successfuly !");
+}
+
